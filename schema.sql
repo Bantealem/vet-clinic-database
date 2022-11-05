3df2aab4eb557f048e7b945e0497ce40b06099b4
Bantealem <bantem2021@gmail.com>
2022-11-05 12:58:19 +0300

    query multiple tables

--- schema.sql
+++ schema.sql
@@ -2,12 +2,12 @@
 
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL,
-    species VARCHAR(100) 
+    species VARCHAR(255) 
 );
 
 CREATE TABLE owners (
